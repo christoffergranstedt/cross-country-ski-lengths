8202c7eb9282884c1a9dbde1d38180f8fbdc688c
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2022-01-26 00:28:19 +0100

    typo

--- server/src/api/controllers/Controller.ts
+++ server/src/api/controllers/Controller.ts
@@ -1,20 +1,15 @@
-import { Application, Router } from 'express'
+import { Router } from 'express'
 
 export abstract class Controller {
   protected router: Router
-  protected appContainer: Application
 
   public constructor () {
     this.router = Router()
   }
 
-  public setApp (app: Application): void {
-    this.appContainer = app
-  }
+  public abstract setRoutes (): void
 
   public getRouter = (): Router => {
     return this.router
   }
-
-  public abstract setRoutes (): void
 }
